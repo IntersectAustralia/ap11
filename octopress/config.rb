81edc0357f62609a220e7dc07e8c36b63c04d22b
Carlos Aya <carlos@intersect.org.au>
2012-08-07 15:35:20 +1000

    Entering github pages using octopress

---
+++ octopress/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/ap11/"
+http_images_path = "/ap11/images"
+http_fonts_path = "/ap11/fonts"
+css_dir = "public/ap11/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
---
+++ octopress/source/index/index.markdown
@@ -0,0 +1,8 @@
+---
+layout: page
+title: "index"
+date: 2012-08-06 16:31
+comments: true
+sharing: true
+footer: true
+---
