e33a29a428701e4acbe78a360710dfc0b768ee8b
Carlos Aya <carlos@intersect.org.au>
2012-10-17 19:59:43 +1100

    Site updated at 2012-10-17 08:59:43 UTC

--- javascripts/asides/github.js
+++ javascripts/asides/github.js
@@ -8,7 +8,39 @@ var github = (function(){
     }
     t.innerHTML = fragment;
   }
+  function renderCommits(target, commits, repo){
+    var i = 0, t = $(target)[0];
+    var fragment = '<li class="nav-header">Commits '+repo+'</li>';
+    if (commits.length > 0) {
+      for(i = 0; i < commits.length; i++) {
+        var web_url = commits[i].url;
+        web_url = web_url.replace("api.github.com/repos","github.com").replace("commits/","commit/");
+        fragment += '<li><a href="'+web_url+'">'+commits[i].commit.message+'</a> by '+commits[i].author.login+'</li>';
+      }
+    } else {
+      fragment += '<li>None</i>';
+    }
+    t.innerHTML = fragment;
+  }
   return {
+    showCommits: function(options){
+      $.getJSON("https://api.github.com/repos/"+options.user+"/"+options.repo+"/commits?since="+options.since+"&until="+options.until, 
+        function(response) {
+          var commits = [];
+          if (!response || response.length == 0) { return; }
+          for (var i = 0; i < response.length; i++) {
+            commits.push(response[i]);
+          }
+          commits.sort(function(a, b) {
+            var aDate = new Date(a.commit.committer.date).valueOf(),
+                bDate = new Date(b.commit.committer.date).valueOf();
+
+            if (aDate === bDate) { return 0; }
+            return aDate > bDate ? -1 : 1;
+          });
+          renderCommits(options.target, commits, options.repo);
+      });
+    },
     showRepos: function(options){
 
       $.getJSON("https://api.github.com/users/"+options.user+"/repos?sort=updated&direction=desc&callback=?", 
